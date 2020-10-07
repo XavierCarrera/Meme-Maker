43e0df3d069b247f2e4ab3f28433fb67959dc7ca
Xavier <xavier.carrera1990@gmail.com>
2020-10-07 14:12:55 -0500

    Final project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar color="red" dark fixed app>
+      <v-toolbar-title>Meme Maker</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <v-layout>
+          <router-view />
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {firestorePlugin} from 'vuefire'  
+import *as firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/storage'
+import router from './router/index'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBozokh9jOaUcLjhlPKo6QpMM5Xm-NFW58",
+  authDomain: "meme-maker-49f58.firebaseapp.com",
+  databaseURL: "https://meme-maker-49f58.firebaseio.com",
+  projectId: "meme-maker-49f58",
+  storageBucket: "meme-maker-49f58.appspot.com",
+  messagingSenderId: "365325207941",
+  appId: "1:365325207941:web:7c8c39a9018b370188dcec",
+  measurementId: "G-9WSS60DTFK"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+export const firestore = firebaseApp.firestore()
+export const storage = firebase.storage().ref('/images')
+
+Vue.use(firestorePlugin)
+
+
 new Vue({
+  vuetify,
   render: h => h(App),
+  router,
+  components: {App} 
 }).$mount('#app')
